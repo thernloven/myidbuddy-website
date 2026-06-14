5dd89cc06c459f3ff68e8132bc89118ce4b8ca74
Lukas Thern Lovén <lukas@thernloven.com>
2026-06-14 16:31:49 +0200

    sitemap

---
+++ app/robots.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://myidbuddy.com";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+  };
+}
---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://myidbuddy.com";
+
+export default function sitemap() {
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
